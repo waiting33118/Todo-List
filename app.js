1eb8558621a6376f4be149bcf7e797cd66f88474
Tony <waiting33118@gmail.com>
2020-05-11 22:43:17 +0800

    feat: add mongoose pkg

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const exphdbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+//connect mongodb
+mongoose.connect('mongodb://localhost/todo-list', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+//取得資料庫連線狀態
+const db = mongoose.connection
+//連線異常
+db.on('error', () => {
+	console.log('mongodb error!')
+})
+//連線成功
+db.once('open', () => {
+	console.log('mongodb connected!')
+})
 
 app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -13,5 +30,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`The Server is running on http://localhost:${port}`)
+	console.log(`The Server is running on http://127.0.0.1:${port}`)
 })
