ecfaaeccb4241b94e7c51a37f235e9f1dd9e7e04
Tony <waiting33118@gmail.com>
2020-05-16 14:38:08 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
 	Todo.find() //取出Todo Model Data
 		.lean() //轉變格式為Javascript Object
+		.sort('_id') //正排序:直接傳入要排序的項目 負排序: 前面加-  EX: -_id
 		.then((todos) => res.render('home', { todos })) //將資料傳入樣板
 		.catch((error) => console.error(error)) //handling error
 })
