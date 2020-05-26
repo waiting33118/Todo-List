d50ad99f82ef47b419e5a5e6b3fb0674001efe22
Tony <waiting33118@gmail.com>
2020-05-26 14:50:10 +0800

    style: comment add white-space

--- app.js
+++ app.js
@@ -2,28 +2,27 @@ const express = require('express')
 const exphdbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const mongoose = require('mongoose')
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
-//模板引擎參數設定
+// 模板引擎參數設定
 app.engine(
-	'handlebars',
-	exphdbs({ defaultLayout: 'main', extname: 'handlebars' }) //可以縮寫，但目前不使用
+  'handlebars',
+  exphdbs({ defaultLayout: 'main', extname: 'handlebars' }) // 可以縮寫，但目前不使用
 )
-//模板啟用
+// 模板啟用
 app.set('view engine', 'handlebars')
-//設定bodyParser 參數
+// 設定bodyParser 參數
 app.use(bodyParser.urlencoded({ extended: true }))
 
-//覆寫前端Method
+// 覆寫前端Method
 app.use(methodOverride('_method'))
 
-//使用路由
+// 使用路由
 app.use(routes)
 
-//伺服器監聽
+// 伺服器監聽
 app.listen(PORT, () => {
-	console.log(`The Server is running on http://127.0.0.1:${PORT}`)
+  console.log(`The Server is running on http://127.0.0.1:${PORT}`)
 })
