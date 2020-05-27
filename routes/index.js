dd9e93633e0253d901569b295cb9dcc8681a398e
Tony <waiting33118@gmail.com>
2020-05-27 11:17:32 +0800

    feat: add new route => users

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const user = require('./modules/user')
 
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', user)
 
 module.exports = router
