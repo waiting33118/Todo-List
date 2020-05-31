d9988bd0aa587de4f6ecda7f492b035b94548a4f
Tony <waiting33118@gmail.com>
2020-05-31 22:30:33 +0800

    feat: initialize & import Passport

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const exphdbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const passport = require('passport')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
 require('./config/mongoose')
 const app = express()
@@ -22,14 +24,26 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 覆寫前端Method
 app.use(methodOverride('_method'))
 
-// 使用session狀態
+// 使用express-session
 app.use(
   session({
-    secret: 'oriforever',
+    secret: 'oriforever is my secret',
     resave: false,
     saveUninitialized: true
   })
 )
+
+// 使用passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./config/passport')(passport)
+
+// 登入後可以取得使用者的資訊方便我們在 view 裡面直接使用
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 // 使用路由
 app.use(routes)
