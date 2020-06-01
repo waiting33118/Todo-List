015b40ee99842ae9e590de1fd5307df32cab5bff
Tony <waiting33118@gmail.com>
2020-06-02 00:31:39 +0800

    refactor: move passport init to passport config

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const session = require('express-session')
 const exphdbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const passport = require('passport')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -34,17 +34,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 覆寫前端Method
 app.use(methodOverride('_method'))
 
-// 使用passport
-app.use(passport.initialize())
-app.use(passport.session())
-
-require('./config/passport')(passport)
-
-// 登入後可以取得使用者的資訊方便我們在 view 裡面直接使用
-app.use((req, res, next) => {
-  res.locals.user = req.user
-  next()
-})
+// 使用passport套件
+usePassport(app)
 
 // 使用路由
 app.use(routes)
