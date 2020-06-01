70698f28c394c13d3ee749064075e4a6c82428fd
Tony <waiting33118@gmail.com>
2020-06-02 00:32:02 +0800

    feat: edit passport config

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,40 @@
+const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 
-module.exports = (passport) => {
+module.exports = (app) => {
+  // 初始化模組
+  app.use(passport.initialize())
+  app.use(passport.session())
+  // 本地策略
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      User.findOne({
-        email: email
-      }).then((user) => {
-        if (!user) {
-          return done(null, false, { message: "The email isn't register yet" })
-        }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password was invalid' })
-        }
-        return done(null, user)
-      })
+      User.findOne({ email })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: '信箱尚未被註冊!'
+            })
+          }
+          if (user.password !== password) {
+            return done(null, false, {
+              message: '密碼錯誤!'
+            })
+          }
+          return done(null, user)
+        })
+        .catch((err) => done(err, false))
     })
   )
-  // 序列化
+  // 序列化&反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
-  // 反序列化
-  passport.deserializeUser((id, done) => {
-    User.findById(id)
-      .lean()
-      .exec((err, user) => {
-        done(err, user)
+  passport.deserializeUser((_id, done) => {
+    User.findById({ _id })
+      .then((user) => {
+        done(null, user)
       })
+      .catch((err) => done(err, null))
   })
 }
